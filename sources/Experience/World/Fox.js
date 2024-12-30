e70062ca7d7e4c5452262716135c5be29ccc99d4
Its-me-ravi <rp808462@gmail.com>
2024-12-30 18:47:53 +0530

    build

--- sources/Experience/World/Fox.js
+++ sources/Experience/World/Fox.js
@@ -3,6 +3,7 @@ import Experience from "../Experience.js";
 
 export default class Fox {
   constructor() {
+    
     this.experience = new Experience();
     this.scene = this.experience.scene;
     this.resources = this.experience.resources;
